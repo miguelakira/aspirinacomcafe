a1f25f55a4a30f2f51babf4072cdf0dcc2c5733e
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2011-08-01 01:08:43 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aspirinacomcafe::Application.config.secret_token = '26863d1e71e422e01a70c9ef268fa7b5b5205070cb0168ae62e3dd69c0174d76ce902fe599f757fc9e0b36da58953223517fe52079d069f245be1ec4f9839454'
